7ebb9957412f8f9eaa366cfb64d22faa59ac621c
yuralevich <shengenminsk@gmail.com>
2023-09-20 17:54:38 +0300

    3-1

--- lesson-03/01.js
+++ lesson-03/01.js
@@ -1,9 +1,7 @@
 const colors = ["синий", "черный", "зеленый", "красный", "желтый", "зеленый"];
 
 function createColorString() {
-    return colors.filter(a => {
-        if (a === "черный" || a === "красный" || a === "желтый") return a
-    }).join('-')
+    return colors.filter(a => a === "черный" || a === "красный" || a === "желтый").join('-')
 }
 
 console.log(createColorString());   
